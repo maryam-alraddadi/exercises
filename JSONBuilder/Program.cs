a8c95012444a711a37f0054cf797fd46dc8b4dac
maryam-alraddadi <maryam.alraddadi@tuwaiq.edu.sa>
2021-04-19 11:57:31 +0300

    updated jsonbuilder

--- JSONBuilder/Program.cs
+++ JSONBuilder/Program.cs
@@ -7,11 +7,11 @@ namespace JSONBuilder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             JSONBuilder j = new JSONBuilder();
                 string res = j.add("key1", "value1")
                 .add("key2", "value2")
                 .add("key3", j.add("nestedKey", "nestedValue3"))
+                .add("key4", 5).add("key6", "value6")
                 .getJSON();
                 Console.WriteLine(res);
         }
@@ -19,12 +19,9 @@ namespace JSONBuilder
 
     class JSONBuilder
     {
-        private Dictionary<string, JSONBuilder> dict = new Dictionary<string, JSONBuilder>();
-        // valueshould be any of string, number, object, array, boolean or null
         private string json;
         public JSONBuilder()
         {
-            //this.dict = new Dictionary<string, JSONBuilder>();
             this.json = "{";
         }
 
@@ -34,6 +31,18 @@ namespace JSONBuilder
             return this;
         }
 
+        public JSONBuilder add(string key, int value)
+        {
+            this.json += "'" + key + "'" + ":" + "'" + value + "'" + " ,";
+            return this;
+        }
+
+        public JSONBuilder add(string key, bool value)
+        {
+            this.json += "'" + key + "'" + ":" + "'" + value + "'" + " ,";
+            return this;
+        }
+        
         public JSONBuilder add(string key, JSONBuilder value)
         {
             this.json += "'" + key + "'" + ":" + "'" + value + "'" + " ,";
